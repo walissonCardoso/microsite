1674cb11fc240f7cf8bc92ca289732f0dcdddf87
walissonGames <walissongomes@mec.gov.br>
2023-02-06 14:51:35 -0300

    Ambiente inicial para iniciar a construção do backend
    Setei o primeiro objetivo de contrução do projeto. Começar construindo algumas APIs no backend para prover o mínimo da funcionalidade de envio de textos

---
+++ backend/src/main/java/br/com/micropensamento/microssite/MicrossiteApplication.java
@@ -0,0 +1,13 @@
+package br.com.micropensamento.microssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrossiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrossiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/micropensamento/microssite/MicrossiteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.micropensamento.microssite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrossiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
