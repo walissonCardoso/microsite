c7639b47df0a963840a25639faab33039d86cba2
walissonGames <walissoncardosogomes@hotmail.com>
2023-02-18 15:47:11 -0300

    Começando o desenvolvimento do front-end com reactjs

--- backend/src/main/java/br/com/micropensamento/microsite/controller/TextoController.java
+++ backend/src/main/java/br/com/micropensamento/microsite/controller/TextoController.java
@@ -1,14 +1,11 @@
 package br.com.micropensamento.microsite.controller;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
---
+++ frontend/microssite/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+            Teste
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
